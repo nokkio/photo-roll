bf249eec8fcf109d6bfbf5670aeb91445643be9e
Brad Daily <brad@bradleyboy.com>
2023-04-03 23:18:32 -0400

    Add gates, disable registrations

--- pages/register.tsx
+++ pages/register.tsx
@@ -2,13 +2,21 @@ import { useRegisterForm } from '@nokkio/auth';
 import { Input } from '@nokkio/forms';
 
 export default function Login() {
-  const { Form, isProcessing } = useRegisterForm();
+  const { Form } = useRegisterForm();
 
   return (
     <Form className="bg-white w-96 mx-auto p-6 space-y-8 shadow">
+      <p className="text-sm bg-yellow-100 p-3 text-yellow-700 border-yellow-200 border">
+        This is a demo application and registration is disabled. View the{' '}
+        <a className="underline" href="https://github.com/nokkio/photo-roll">
+          readme in the repo
+        </a>{' '}
+        for instructions on running this locally and re-enabling registrations.
+      </p>
       <fieldset className="flex flex-col space-y-2">
         <label className="text-xs uppercase text-gray-500">Username</label>
         <Input
+          disabled
           autoFocus
           type="text"
           name="username"
@@ -19,6 +27,7 @@ export default function Login() {
       <fieldset className="flex flex-col space-y-2">
         <label className="text-xs uppercase text-gray-500">Password</label>
         <Input
+          disabled
           type="password"
           name="password"
           placeholder="password"
@@ -29,6 +38,7 @@ export default function Login() {
       <fieldset className="flex flex-col space-y-2">
         <label className="text-xs uppercase text-gray-500">Avatar</label>
         <Input
+          disabled
           type="file"
           name="avatar"
           className="py-1 text-xs focus:outline-none focus:border-indigo-700"
@@ -36,7 +46,7 @@ export default function Login() {
       </fieldset>
 
       <button
-        disabled={isProcessing}
+        disabled
         className="bg-indigo-700 w-full p-3 text-indigo-50 rounded disabled:bg-gray-400"
       >
         Sign up
---
+++ server/boot.ts
@@ -0,0 +1,42 @@
+import { User, Photo, Like } from '@nokkio/magic';
+import { NotAuthorizedError } from '@nokkio/errors';
+
+export default async function () {
+  User.beforeCreate('RESTRICT_TO_ENDPOINTS');
+  User.beforeDelete('RESTRICT_TO_ENDPOINTS');
+  User.beforeUpdate('RESTRICT_TO_ENDPOINTS');
+
+  User.beforeFind(async ({ query, isTrusted }) => {
+    if (isTrusted) {
+      return query;
+    }
+
+    if (!query.id) {
+      throw new NotAuthorizedError();
+    }
+
+    return query;
+  });
+
+  Photo.beforeCreate(async ({ fields, userId }) => {
+    if (!userId || fields.userId !== userId) {
+      throw new NotAuthorizedError();
+    }
+
+    return fields;
+  });
+
+  Photo.beforeUpdate('RESTRICT_TO_ENDPOINTS');
+  Photo.beforeDelete('RESTRICT_TO_ENDPOINTS');
+
+  Like.beforeCreate(async ({ userId, fields }) => {
+    if (!userId || fields.userId !== userId) {
+      throw new NotAuthorizedError();
+    }
+
+    return fields;
+  });
+
+  Like.beforeUpdate('RESTRICT_TO_ENDPOINTS');
+  Like.beforeDelete('RESTRICT_TO_ENDPOINTS');
+}
